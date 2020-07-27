dd04041d2a4fe28586b0ec0086a5b4ade607c42b
Smriti Malakar <68274604+smritimalakar59174@users.noreply.github.com>
2020-07-27 19:59:05 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,9 @@
+function checkForm() {
+    //  input validation
+ }
+ 
+ document.getElementById("submit").addEventListener("click", function(event) {
+  checkForm();
+event.preventDefault();
+ 
+ });
